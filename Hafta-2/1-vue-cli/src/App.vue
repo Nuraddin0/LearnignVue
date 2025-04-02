23229f1e841eb521c61a29a8d149aaa4d9c8a957
Nureddin <nuraddinabbas@gmail.com>
2025-04-02 22:22:42 +0300

    Intro to vue-cli

---
+++ Hafta-2/1-vue-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  
+  <div class="container">
+    <app-header></app-header>
+    <h1>Welcome to Vue World</h1>
+    <CounterItem/><counter-item/>
+    <count-item></count-item><!--same as <CounterItem/><counter-item/> (vue-cli makes auto convertion)-->
+  </div>
+  
+</template>
+
+<script>
+import counterItem from './components/CounterItem.vue'
+export default{
+  components:{
+    CounterItem: counterItem,
+  }
+}
+</script>
