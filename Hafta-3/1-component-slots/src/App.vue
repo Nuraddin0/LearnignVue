aee7f11453bfc41ee5331354d8112a9763a7a794
Nureddin <nuraddinabbas@gmail.com>
2025-04-04 13:22:03 +0300

    component-slots

---
+++ Hafta-3/1-component-slots/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <MyModal :onSave="onSave">
+      <template #title>
+        <h3>Slot ile gelen title</h3>
+      </template>
+
+
+      <template v-slot:content>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laborum veritatis nulla, odio repellat quasi
+        harum? Eos, aspernatur accusantium illo alias sapiente rerum ex voluptatibus eum in reprehenderit, optio vel!
+      </template>
+      <!-- <template #content>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laborum veritatis nulla, odio repellat quasi
+        harum? Eos, aspernatur accusantium illo alias sapiente rerum ex voluptatibus eum in reprehenderit, optio vel!
+      </template> -->
+
+
+      <template #default>
+        Bu default olarak gosterilecek
+      </template>
+      
+    </MyModal>
+  </div>
+</template>
+
+<script>
+import MyModal from '@/components/MyModal.vue'
+export default {
+  components: {
+    MyModal
+  },
+  methods: {
+    onSave() {
+      alert("Saved!!")
+    }
+  }
+}</script>
---
+++ Hafta-3/1-component-slots/src/components/MyModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="title"/>
+        </div>
+        <hr>
+        <div class="content">
+            <slot name="content"></slot>
+        </div>
+        <hr>
+        <div class="footer text-right">
+            <button class="red">Kapat</button>
+            <button class="green" @click="onSave">Kaydet</button>
+        </div>
+
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['onSave']
+}</script>
